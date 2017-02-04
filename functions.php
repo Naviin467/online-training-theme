9f5a536860b453c9cee7aca1c1048b9a690f2193
Jason Taylor <jason@jasongtaylor.com>
2017-02-04 17:35:29 -0500

    Adding Questions and Tutorials

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@ if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
 endif;
 add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
 
-function ocs_register_post_type() {
+function ocs_register_course_post_type() {
 
 	$singular = 'Course';
 	$plural = 'Courses';
@@ -67,7 +67,120 @@ function ocs_register_post_type() {
 	);
 	register_post_type( $slug, $args );
 }
-add_action( 'init', 'ocs_register_post_type' );
+add_action( 'init', 'ocs_register_course_post_type' );
 
+function ocs_register_question_post_type() {
+
+	$singular = 'Question';
+	$plural = 'Questions';
+	$slug = str_replace( ' ', '_', strtolower( $singular ) );
+	$labels = array(
+		'name' 			=> $plural,
+		'singular_name' 	=> $singular,
+		'add_new' 		=> 'Add New',
+		'add_new_item'  	=> 'Add New ' . $singular,
+		'edit'		        => 'Edit',
+		'edit_item'	        => 'Edit ' . $singular,
+		'new_item'	        => 'New ' . $singular,
+		'view' 			=> 'View ' . $singular,
+		'view_item' 		=> 'View ' . $singular,
+		'search_term'   	=> 'Search ' . $plural,
+		'parent' 		=> 'Parent ' . $singular,
+		'not_found' 		=> 'No ' . $plural .' found',
+		'not_found_in_trash' 	=> 'No ' . $plural .' in Trash'
+		);
+	$args = array(
+		'labels'              => $labels,
+	        'public'              => true,
+	        'publicly_queryable'  => true,
+	        'exclude_from_search' => false,
+	        'show_in_nav_menus'   => true,
+	        'show_ui'             => true,
+	        'show_in_menu'        => true,
+	        'show_in_admin_bar'   => true,
+	        'menu_position'       => 10,
+	        'menu_icon'           => 'dashicons-format-chat',
+	        'can_export'          => true,
+	        'delete_with_user'    => false,
+	        'hierarchical'        => false,
+	        'has_archive'         => true,
+	        'query_var'           => true,
+	        'capability_type'     => 'post',
+	        'map_meta_cap'        => true,
+	        // 'capabilities' => array(),
+	        'rewrite'             => array(
+	        	'slug' => $slug,
+	        	'with_front' => true,
+	        	'pages' => true,
+	        	'feeds' => true,
+	        ),
+	        'supports'            => array(
+	        	'title',
+	        	'editor',
+	        	'author',
+	        	'custom-fields',
+            'wpcom-markdown'
+	        )
+	);
+	register_post_type( $slug, $args );
+}
+add_action( 'init', 'ocs_register_question_post_type' );
+
+function ocs_register_tutorial_post_type() {
+
+	$singular = 'Tutorial';
+	$plural = 'Tutorials';
+	$slug = str_replace( ' ', '_', strtolower( $singular ) );
+	$labels = array(
+		'name' 			=> $plural,
+		'singular_name' 	=> $singular,
+		'add_new' 		=> 'Add New',
+		'add_new_item'  	=> 'Add New ' . $singular,
+		'edit'		        => 'Edit',
+		'edit_item'	        => 'Edit ' . $singular,
+		'new_item'	        => 'New ' . $singular,
+		'view' 			=> 'View ' . $singular,
+		'view_item' 		=> 'View ' . $singular,
+		'search_term'   	=> 'Search ' . $plural,
+		'parent' 		=> 'Parent ' . $singular,
+		'not_found' 		=> 'No ' . $plural .' found',
+		'not_found_in_trash' 	=> 'No ' . $plural .' in Trash'
+		);
+	$args = array(
+		'labels'              => $labels,
+	        'public'              => true,
+	        'publicly_queryable'  => true,
+	        'exclude_from_search' => false,
+	        'show_in_nav_menus'   => true,
+	        'show_ui'             => true,
+	        'show_in_menu'        => true,
+	        'show_in_admin_bar'   => true,
+	        'menu_position'       => 10,
+	        'menu_icon'           => 'dashicons-book-alt',
+	        'can_export'          => true,
+	        'delete_with_user'    => false,
+	        'hierarchical'        => false,
+	        'has_archive'         => true,
+	        'query_var'           => true,
+	        'capability_type'     => 'post',
+	        'map_meta_cap'        => true,
+	        // 'capabilities' => array(),
+	        'rewrite'             => array(
+	        	'slug' => $slug,
+	        	'with_front' => true,
+	        	'pages' => true,
+	        	'feeds' => true,
+	        ),
+	        'supports'            => array(
+	        	'title',
+	        	'editor',
+	        	'author',
+	        	'custom-fields',
+            'wpcom-markdown'
+	        )
+	);
+	register_post_type( $slug, $args );
+}
+add_action( 'init', 'ocs_register_tutorial_post_type' );
 
 // END ENQUEUE PARENT ACTION
