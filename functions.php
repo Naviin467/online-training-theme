1744cfe1c3c7db4fd42f6d261aaeb0d0fd1259e9
Jason Taylor <jason@jasongtaylor.com>
2017-02-04 18:19:30 -0500

    Adding custom taxonomies

--- functions.php
+++ functions.php
@@ -12,6 +12,7 @@ if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
 endif;
 add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
 
+// Register Custom Post Types
 function ocs_register_course_post_type() {
 
 	$singular = 'Course';
@@ -182,5 +183,79 @@ function ocs_register_tutorial_post_type() {
 	register_post_type( $slug, $args );
 }
 add_action( 'init', 'ocs_register_tutorial_post_type' );
+
+// Register Custom taxonomies
+function ocs_register_question_category_taxonomy() {
+  // set up labels
+	$labels = array(
+		'name'              => 'Question Categories',
+		'singular_name'     => 'Question Category',
+		'search_items'      => 'Search Question Categories',
+		'all_items'         => 'All Question Categories',
+		'edit_item'         => 'Edit Question Category',
+		'update_item'       => 'Update Question Category',
+		'add_new_item'      => 'Add New Question Category',
+		'new_item_name'     => 'New Question Category',
+		'menu_name'         => 'Question Categories'
+	);
+	// register taxonomy
+	register_taxonomy( 'questioncat', 'question', array(
+		'hierarchical' => true,
+		'labels' => $labels,
+		'query_var' => true,
+		'show_admin_column' => true
+	) );
+}
+add_action( 'init', 'ocs_register_question_category_taxonomy' );
+
+function ocs_register_tutorial_category_taxonomy() {
+  // set up labels
+	$labels = array(
+		'name'              => 'Tutorial Categories',
+		'singular_name'     => 'Tutorial Category',
+		'search_items'      => 'Search Tutorial Categories',
+		'all_items'         => 'All Tutorial Categories',
+		'edit_item'         => 'Edit Tutorial Category',
+		'update_item'       => 'Update Tutorial Category',
+		'add_new_item'      => 'Add New Tutorial Category',
+		'new_item_name'     => 'New Tutorial Category',
+		'menu_name'         => 'Tutorial Categories'
+	);
+	// register taxonomy
+	register_taxonomy( 'tutcat', 'tutorial', array(
+		'hierarchical' => true,
+		'labels' => $labels,
+		'query_var' => true,
+		'show_admin_column' => true
+	) );
+}
+add_action( 'init', 'ocs_register_tutorial_category_taxonomy' );
+
+function ocs_register_tutorial_topics_taxonomy() {
+  // set up labels
+	$labels = array(
+		'name'              => 'Tutorial Topics',
+		'singular_name'     => 'Tutorial Topic',
+		'search_items'      => 'Search Tutorial Topics',
+		'all_items'         => 'All Tutorial Topics',
+		'edit_item'         => 'Edit Tutorial Topic',
+		'update_item'       => 'Update Tutorial Topic',
+		'add_new_item'      => 'Add New Tutorial Topic',
+		'new_item_name'     => 'New Tutorial Topic',
+		'menu_name'         => 'Tutorial Topics'
+	);
+
+  // register taxonomy
+	register_taxonomy( 'tuttopic', 'tutorial', array(
+		'hierarchical' => false,
+		'labels' => $labels,
+		'query_var' => true,
+		'show_admin_column' => true
+	) );
+}
+add_action( 'init', 'ocs_register_tutorial_topics_taxonomy' );
+
+
+
 
 // END ENQUEUE PARENT ACTION
