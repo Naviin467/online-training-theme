02f34fe6e2b9cf96fc620993ecd4205021a969be
Jason Taylor <jason@jasongtaylor.com>
2017-02-04 22:11:43 -0500

    Fix problem with theme

--- functions.php
+++ functions.php
@@ -30,63 +30,6 @@ function ocs_taxonomy_config( $singular, $plural, $hierarchical ) {
 		'menu_name'         => $plural
 	);
 
-  function ocs_register_post_type( $singular, $plural, $icon ) {
-  	$slug = str_replace( ' ', '_', strtolower( $singular ) );
-  	$labels = array(
-  		'name' 			=> $plural,
-  		'singular_name' 	=> $singular,
-  		'add_new' 		=> 'Add New',
-  		'add_new_item'  	=> 'Add New ' . $singular,
-  		'edit'		        => 'Edit',
-  		'edit_item'	        => 'Edit ' . $singular,
-  		'new_item'	        => 'New ' . $singular,
-  		'view' 			=> 'View ' . $singular,
-  		'view_item' 		=> 'View ' . $singular,
-  		'search_term'   	=> 'Search ' . $plural,
-  		'parent' 		=> 'Parent ' . $singular,
-  		'not_found' 		=> 'No ' . $plural .' found',
-  		'not_found_in_trash' 	=> 'No ' . $plural .' in Trash'
-  		);
-  	$args = array(
-  		'labels'              => $labels,
-  	        'public'              => true,
-  	        'publicly_queryable'  => true,
-  	        'exclude_from_search' => false,
-  	        'show_in_nav_menus'   => true,
-  	        'show_ui'             => true,
-  	        'show_in_menu'        => true,
-  	        'show_in_admin_bar'   => true,
-  	        'menu_position'       => 10,
-  	        'menu_icon'           => $icon,
-  	        'can_export'          => true,
-  	        'delete_with_user'    => false,
-  	        'hierarchical'        => false,
-  	        'has_archive'         => true,
-  	        'query_var'           => true,
-  	        'capability_type'     => 'post',
-  	        'map_meta_cap'        => true,
-  	        // 'capabilities' => array(),
-  	        'rewrite'             => array(
-  	        	'slug' => $slug,
-  	        	'with_front' => true,
-  	        	'pages' => true,
-  	        	'feeds' => true,
-  	        ),
-  	        'supports'            => array(
-  	        	'title',
-  	        	'editor',
-  	        	'author',
-  	        	'custom-fields',
-              'wpcom-markdown'
-  	        )
-  	);
-
-    return $args;
-
-  	register_post_type( $slug, $args );
-  }
-
-
   // register taxonomy
 	return array(
 		'hierarchical' => $hierarchical,
@@ -94,6 +37,62 @@ function ocs_taxonomy_config( $singular, $plural, $hierarchical ) {
 		'query_var' => true,
 		'show_admin_column' => true
 	);
+}
+
+function ocs_register_post_type( $singular, $plural, $icon ) {
+  $slug = str_replace( ' ', '_', strtolower( $singular ) );
+  $labels = array(
+    'name' 			=> $plural,
+    'singular_name' 	=> $singular,
+    'add_new' 		=> 'Add New',
+    'add_new_item'  	=> 'Add New ' . $singular,
+    'edit'		        => 'Edit',
+    'edit_item'	        => 'Edit ' . $singular,
+    'new_item'	        => 'New ' . $singular,
+    'view' 			=> 'View ' . $singular,
+    'view_item' 		=> 'View ' . $singular,
+    'search_term'   	=> 'Search ' . $plural,
+    'parent' 		=> 'Parent ' . $singular,
+    'not_found' 		=> 'No ' . $plural .' found',
+    'not_found_in_trash' 	=> 'No ' . $plural .' in Trash'
+    );
+  $args = array(
+    'labels'              => $labels,
+          'public'              => true,
+          'publicly_queryable'  => true,
+          'exclude_from_search' => false,
+          'show_in_nav_menus'   => true,
+          'show_ui'             => true,
+          'show_in_menu'        => true,
+          'show_in_admin_bar'   => true,
+          'menu_position'       => 10,
+          'menu_icon'           => $icon,
+          'can_export'          => true,
+          'delete_with_user'    => false,
+          'hierarchical'        => false,
+          'has_archive'         => true,
+          'query_var'           => true,
+          'capability_type'     => 'post',
+          'map_meta_cap'        => true,
+          // 'capabilities' => array(),
+          'rewrite'             => array(
+            'slug' => $slug,
+            'with_front' => true,
+            'pages' => true,
+            'feeds' => true,
+          ),
+          'supports'            => array(
+            'title',
+            'editor',
+            'author',
+            'custom-fields',
+            'wpcom-markdown'
+          )
+  );
+
+  return $args;
+
+  register_post_type( $slug, $args );
 }
 
 // Register Custom Post Types
